63989a86776ed512c644a27952e5384254c3485a
ioav <ioavlivneh@gmail.com>
2019-07-17 10:14:15 +0200

    ioav-livneh

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,36 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
+app.get(['/home', "/"], function (req, res) {
+  res.render('home')
+})
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
+app.get('/beers', function (req, res) {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers)
+    res.render('beers.hbs', {beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
 
+app.get('/random-beer', function (req, res) {
+  punkAPI.getRandom()
+  .then(beer => {
+    console.log(beer)
+    res.render('random-beer.hbs', {beer: beer[0]})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+})
 
 
 app.listen(3000);
---
+++ starter_code/public/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(event) {
+
+    $("#btn-beers").click(function() {
+        window.location.assign('beers');
+      });
+      $("#btn-random-beer").click(function() {
+        window.location.assign('random-beer');
+      });
+
+})
